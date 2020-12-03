45bcf0bb10684b8f6507179a74f7d5482769d526
Matthew Harris <matthewharrisak@gmail.com>
2020-12-03 16:44:07 -0600

    set up get request and connected to python server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  // state is creating our event object - the values will be populated on the form in the Render() section of this component
+    state={
+        new_pet:{
+            name: '',
+            breed: '',
+            color: '',
+            is_checked_in: '',
+        }
+    }
+
+    componentDidMount = () => {
+      this.getPet();
+    }
+
+      // this function will handle the values from the event form and use those values to setState
+    handleChange= (keyname, event) => {
+        event.preventDefault();
+        
+        this.setState({
+          new_pet:{
+            ...this.state.new_pet,
+            [keyname]: event.target.value,
+        }});
+     }
+
+      // onClick this function will fire off our new state object
+     addPet = () => {
+         console.log('this is our state' , this.state);
+         this.props.dispatch({ type: 'NEW_PET' , payload: this.state});
+    }
+
+    getPet = () => {
+      console.log('this is our state getting all the pets');
+      this.props.dispatch({ type: 'FETCH_PETS'});
+    }
+
+  render() {
+    return (
+      <div>
+          <form onSubmit={this.addPet}>
+              <h1> NEW PET </h1>
+
+                <input required onChange={(event) => this.handleChange( 'name' , event)} 
+                    type="text" id="" placeholder='name'/>
+
+                <input onChange={(event) => this.handleChange( 'breed' , event)} 
+                                    type="text" id="" placeholder='breed'/>
+
+                <input  onChange={(event) => this.handleChange( 'color' , event)} 
+                                    type="text" id="color" placeholder='color'/>
+
+                <input onChange={(event) => this.handleChange( 'is_checked_in' , event)} 
+                                                    type="text" id="is_checked_in" placeholder='is_checked_in'/>
+                  <button className='newPet'> submit</button>
+           
+           
+                  
+          </form>
+
+          {JSON.stringify(this.props.store.petReducer)}
+          </div>
+    );
+  }
 }
 
-export default App;
+// Redux
+const mapStoreToProps = (store) => ({
+    store
+});
+
+// this allows us to use <App /> in index.js
+export default connect(mapStoreToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,16 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
+
+const sagaMiddleware = createSagaMiddleware();
+
+function* getPets() {
+  const petResponse = yield axios.get('/pet');
+  yield put({type: 'SET_PETS', payload: petResponse.data});
+}
+
+const petReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_PETS':
+      return action.payload;
+    default: 
+      return state;
+  }
+}
+
+function* rootSaga() {
+  yield takeEvery('FETCH_PETS', getPets);
+}
+
+const storeInstance = createStore(
+  combineReducers({
+    petReducer
+  }),
+  applyMiddleware(sagaMiddleware, logger)
+)
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={storeInstance}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
